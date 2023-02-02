d5597ff32c32c3a434762ab70633e184ff0c7a6c
LeandroKastilho <119023278+LeandroKastilho@users.noreply.github.com>
2023-02-02 16:07:55 -0300

    Verificação do e-mail
    Comecei a trabalhar na verificação do e-mail.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function enviar() {
-    let 
+    let ademail = document.querySelector('#endemail')
+    
+    if (ademail.lenght == null) {
+
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+
+        window.alert('tudo ok Prossiga')
+    }
 }
